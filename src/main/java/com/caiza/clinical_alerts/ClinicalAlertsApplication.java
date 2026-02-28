ea5f7e7e9d0bf42ed5a56ad4a2d07837ea540f64
caiza fortunato <caizafortunato27@gmail.com>
2026-02-28 19:19:12 -0300

    Initial commit

---
+++ src/main/java/com/caiza/clinical_alerts/ClinicalAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.caiza.clinical_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiza/clinical_alerts/ClinicalAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiza.clinical_alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
