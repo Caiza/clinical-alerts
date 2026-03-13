e4c7ab3f646071cc8b0f544b21ba6a0fb0e1c751
caiza fortunato <caizafortunato27@gmail.com>
2026-03-13 10:58:23 -0300

    Exception updated

--- src/main/java/com/caiza/clinical_alerts/exception/GlobalExceptionHandle.java
+++ src/main/java/com/caiza/clinical_alerts/exception/GlobalExceptionHandle.java
@@ -2,8 +2,11 @@ package com.caiza.clinical_alerts.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 
@@ -37,5 +40,38 @@ public class GlobalExceptionHandle {
                 LocalDateTime.now()
         );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiError> HandleNoResourceFound(NoResourceFoundException ex){
+        ApiError error = new ApiError(
+                "Path not found " + ex.getResourcePath(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleArgumentNotValid(MethodArgumentNotValidException ex){
+        ApiError error = new ApiError(
+                "Invalid value for parameter: '" + ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiError> handleTypeMismatch(MethodArgumentTypeMismatchException ex){
+       String message = String.format("Invalid value '%s' for parameter '%s'. Expected type: %s",
+               ex.getValue(), ex.getName(), ex.getRequiredType() != null? ex.getRequiredType().getSimpleName() : "unknown"
+       );
+        ApiError error = new ApiError(
+                message,
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/caiza/clinical_alerts/patient/PatientService.java
+++ src/main/java/com/caiza/clinical_alerts/patient/PatientService.java
@@ -4,11 +4,8 @@ import com.caiza.clinical_alerts.exception.BusinessException;
 import com.caiza.clinical_alerts.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-
 import org.springframework.data.domain.Pageable;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
